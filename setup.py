c510e48e9fe6b6365fb1ffb017d3a7daa359dba4
Ricardo Sanginés <rickrebel@gmail.com>
2025-11-12 13:51:30 -0600

    cambio la versión

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yeeko_xlsx_export',
-    version='0.0.2',
+    version='0.0.3',
     description='generic xlsx export tool',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
